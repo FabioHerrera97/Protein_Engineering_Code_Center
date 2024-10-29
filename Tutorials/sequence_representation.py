c735fa14d30464e89dc972402461ef7da1df32c2
Herrera Rocha <fabioesteban97@hotmail.com>
2024-10-29 16:24:56 +0100

    Create representation calculation script

--- Tutorials/sequence_representation.py
+++ Tutorials/sequence_representation.py
@@ -4,8 +4,7 @@ import argparse
 import sys 
 
 sys.path.insert(1, '/raid/data/fherrera/Protein_Engineering_Code_Center/Src/')
-from numerical_representation import OneHotEncoding, SequenceRepresentation, EmbeddingGenerator
-
+from numerical_representation import OneHotEncoding, IfeatproEncoding, Esm1v_Encoding, Prott5Encoding
 
 
 def get_representations(data_file, seq_column, feature_types, output_dir):
@@ -29,49 +28,41 @@ def get_representations(data_file, seq_column, feature_types, output_dir):
     data = pd.read_csv(data_file)
     sequences = data[seq_column]
 
-    if 'one_hot' in feature_types:
+    if 'one_hot' or 'all' in feature_types:
 
         one_hot_encoder = OneHotEncoding()
         one_hot_representations = [one_hot_encoder.encode(sequence) for sequence in sequences]
         df_one_hot = pd.DataFrame(one_hot_representations)
         df_one_hot.to_csv(f'{output_dir}/one_hot.csv', index=False)
+        print('One hot encoding done!')
 
 
-    '''
 
     if 'ifeatpro' or 'all' in feature_types:
-        ifeatpro_features = [SequenceRepresentation.get_ifeatpro_features(sequence, output_file) for sequence in sequences]
+        ifeatpro_encoder = IfeatproEncoding(output_dir)
+        ifeatpro_encoder.get_fasta_file(sequences)
+        ifeatpro_features = [ifeatpro_encoder.get_ifeatpro_features(sequence) for sequence in sequences]
         ifeatpro_features_df = pd.DataFrame(ifeatpro_features)
-        out_file = f'{output_file}_ifeatpro.pkl'
-        ifeatpro_features_df.loc[:, 'Ids', 'Log_expression'] = ids, label  
-        ifeatpro_features_df.to_pickle(out_file, index=False) 
-          
-
-    if 'aaindex' or 'all' in feature_types:
-        aaindex_features = [SequenceRepresentation.get_aaindex(sequence) for sequence in sequences]
-        aaindex_features_df = pd.DataFrame(aaindex_features)
-        out_file = f'{output_file}_aaindex.pkl'
-        aaindex_features_df.loc[:, 'Ids', 'Log_expression'] = ids, label   
-        aaindex_features_df.to_pickle(out_file, index=False)  
+        ifeatpro_features_df.to_csv(f'{output_dir}/ifeatpro.csv', index=False)
+        print('Ifeatpro encoding done!')
 
     if 'esmv1' or 'all' in feature_types:
-        esmv1_features = [EmbeddingGenerator.get_esmv1_embedding(sequence) for sequence in sequences]
+        esm1v_encoder = Esm1v_Encoding()
+        esmv1_features = [esm1v_encoder.get_esmv1_embedding(sequence) for sequence in sequences]
         esmv1_features_df = pd.DataFrame(esmv1_features)
-        out_file = f'{output_file}_esmv1.pkl'
-        esmv1_features_df.loc[:, 'Ids', 'Log_expression'] = ids, label
-        esmv1_features_df.to_pickle(out_file, index=False)
+        esmv1_features_df.to_csv(f'{output_dir}/esmv1.csv', index=False)
+        print('Esmv1 encoding done!')
 
     if 'prott5' or 'all' in feature_types:
-        prot5_features = [EmbeddingGenerator.get_prott5_embedding(sequence) for sequence in sequences]
+        prott5_encoder = Prott5Encoding()
+        prot5_features = [prott5_encoder.get_prott5_embedding(sequence) for sequence in sequences]
         prot5_features_df = pd.DataFrame(prot5_features)
-        out_file = f'{output_file}_prott5.pkl'
-        prot5_features_df.loc[:, 'Ids', 'Log_expression'] = ids, label 
-        prot5_features_df.to_pickle(out_file, index=False)
+        prot5_features_df.to_csv(f'{output_dir}/prott5.csv', index=False)
+        print('Prott5 encoding done!')
 
     else:
-        raise ValueError('Invalid feature type. Please choose from one_hot, ifeatpro, aaindex, esmv1, prott5, or all.')'''
-
-
+        raise ValueError('Invalid feature type. Please choose from one_hot, ifeatpro, aaindex, esmv1, prott5, or all.')
+    
 def main():
     parser = argparse.ArgumentParser(description='Generate numerical representations for protein sequences.')
     parser.add_argument('--data_file', type=str, help='Path to the data file containing sequences.')
