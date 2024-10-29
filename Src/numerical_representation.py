ebf01683a9822e0d189627356bb7db892d3b54ac
Herrera Rocha <fabioesteban97@hotmail.com>
2024-10-29 16:09:22 +0100

    Add representation classes

--- Src/numerical_representation.py
+++ Src/numerical_representation.py
@@ -1,10 +1,8 @@
 from ifeatpro.features import get_all_features
-from aaindex import aaindex1
 from transformers import T5Tokenizer, T5EncoderModel
 from sklearn.preprocessing import OneHotEncoder
 import torch
 import esm 
-import numpy as np
 
 class OneHotEncoding:
 
@@ -14,93 +12,30 @@ class OneHotEncoding:
     def encode(self, sequence):
         sequence = [[aa] for aa in sequence]
         return self.encoder.fit_transform(sequence).flatten()
+    
+class IfeatproEncoding:
 
-class SequenceRepresentation:
-    """
-    Represents a sequence and provides methods for numerical representation.
-    """
-
-    def __init__(self, sequence, output_dir):  
-        """
-        Initializes a SequenceRepresentation object.
-
-        Parameters:
-        sequence (str): The sequence to be represented numerically.
-        """
-        self.sequence = sequence
+    def __init__(self, output_dir):
         self.output_dir = output_dir
 
-    def one_hot_encoding(sequence):
-        """
-        Performs one-hot encoding on the sequence.
+    def get_fasta_file(self, sequences):
+        with open(f'{self.output_dir}/sequences.fasta', 'w') as f:
+            for i, seq in enumerate(sequences):
+                f.write(f'>seq{i}\n{seq}\n')
 
-        Returns:
-        numpy.ndarray: The one-hot encoded matrix representation of the sequence.
-        """
-        amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
-        aa_to_index = dict((c, i) for i, c in enumerate(amino_acids))
+    def get_ifeatpro_features(self):
+        get_all_features(f'{self.output_dir}/sequences.fasta', self.output_dir)
 
-        one_hot_matrix = np.zeros((len(sequence), len(amino_acids)))
-        for i, aa in enumerate(sequence):
-            one_hot_matrix[i, aa_to_index[aa]] = 1
-        
-        return one_hot_matrix
-    
-    def get_ifeatpro_features(sequence, output_dir):
-        """
-        Retrieves the IFeatPro features for the sequence.
-
-        Returns:
-        list: The IFeatPro features for each amino acid in the sequence.
-        """
-        return get_all_features(sequence, output_dir)
-    
-    def get_aaindex(sequence):
-        """
-        Retrieves the AAIindex values for the sequence.
-
-        Returns:
-        list: The AAIindex values for each amino acid in the sequence.
-        """
-        aaindex_values = []
-        for aa in sequence:
-            aaindex_values.append(aaindex1[aa])
-        return aaindex_values
-
-class EmbeddingGenerator:
-    """
-    Class for generating embeddings using ESM-1V and ProtT5-XL-UniRef50 models.
-    """
+class Esm1v_Encoding:
 
     def __init__(self, device='cuda'):
-        """
-        Initializes the EmbeddingGenerator.
-
-        Args:
-            device (str): Device to use for computation. Defaults to 'cuda' if available, otherwise 'cpu'.
-        """
         self.device = torch.device(device if torch.cuda.is_available() else 'cpu')
 
-        # Load ESM-1V model
-        self.esm_model, self.esm_alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+        self.esm_model, self.esm_alphabet = esm.pretrained.esm1v_t33_650M_UR90S()
         self.esm_model = self.esm_model.to(self.device)
         self.batch_converter = self.esm_alphabet.get_batch_converter()
 
-        # Load ProtT5-XL-UniRef50 model
-        self.t5_tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_uniref50')
-        self.t5_model = T5EncoderModel.from_pretrained('Rostlab/prot_t5_xl_uniref50')
-        self.t5_model = self.t5_model.to(self.device)
-
-    def get_esmv1_embedding(self, sequence):
-        """
-        Generates ESM-1V embedding for a given sequence.
-
-        Args:
-            sequence (str): Input sequence.
-
-        Returns:
-            numpy.ndarray: ESM-1V embedding of the sequence.
-        """
+    def calculate_esmv1_embedding(self, sequence):
         data = [(f'seq1', sequence)]
         batch_labels, batch_strs, batch_tokens = self.batch_converter(data)
         batch_tokens = batch_tokens.to(self.device)
@@ -111,16 +46,21 @@ class EmbeddingGenerator:
             sequence_embedding = token_representations.mean(1).cpu().numpy()
         return sequence_embedding
     
-    def get_prott5_embedding(self, sequence):
-        """
-        Generates ProtT5-XL-UniRef50 embedding for a given sequence.
+    def generate_esm1v_embedding(self, sequence):
+        esm1v_embedding = self.calculate_esmv1_embedding(sequence)
+        return esm1v_embedding
 
-        Args:
-            sequence (str): Input sequence.
 
-        Returns:
-            numpy.ndarray: ProtT5-XL-UniRef50 embedding of the sequence.
-        """
+class Prott5Encoding:
+    
+    def __init__(self, device='cuda'):
+        self.device = torch.device(device if torch.cuda.is_available() else 'cpu')
+
+        self.t5_tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_uniref50')
+        self.t5_model = T5EncoderModel.from_pretrained('Rostlab/prot_t5_xl_uniref50')
+        self.t5_model = self.t5_model.to(self.device)
+    
+    def calculate_prott5_embedding(self, sequence):
         inputs = self.t5_tokenizer(sequence, return_tensors='pt')
         inputs = {key: val.to(self.device) for key, val in inputs.items()}
 
@@ -129,3 +69,7 @@ class EmbeddingGenerator:
 
         sequence_embedding = outputs.last_hidden_state.mean(1).cpu().numpy()
         return sequence_embedding
+    
+    def generate_prott5_embedding(self, sequence):
+        prott5_embedding = self.calculate_prott5_embedding(sequence)
+        return prott5_embedding
