7a35fa39079d5781ebf3c8ee96ffa6877b4c1199
Herrera Rocha <fabioesteban97@hotmail.com>
2024-10-30 11:42:14 +0100

    Update embedding functions

--- Tutorials/sequence_representation.py
+++ Tutorials/sequence_representation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import argparse
 import sys 
+import h5py
 
 sys.path.insert(1, '/raid/data/fherrera/Protein_Engineering_Code_Center/Src/')
 from numerical_representation import OneHotEncoding, IfeatproEncoding, Esm1v_Encoding, Prott5Encoding
@@ -44,15 +45,17 @@ def get_representations(data_file, seq_column, feature_types, output_dir):
     if 'esm1v' in feature_types or 'all' in feature_types:
         esm1v_encoder = Esm1v_Encoding()
         esmv1_features = [esm1v_encoder.generate_esm1v_embedding(sequence) for sequence in sequences]
-        esmv1_features_df = pd.DataFrame(esmv1_features)
-        esmv1_features_df.to_csv(f'{output_dir}/esmv1.csv', index=False)
+        esmv1_features = np.array(esmv1_features)
+        with h5py.File(f'{output_dir}/esm1v.h5', 'w') as f:
+            f.create_dataset('esm1v', data=esmv1_features)
         print('Esmv1 encoding done!')
 
     if 'prott5' in feature_types or 'all' in feature_types:
         prott5_encoder = Prott5Encoding()
-        prot5_features = [prott5_encoder.generate_prott5_embedding(sequence) for sequence in sequences]
-        prot5_features_df = pd.DataFrame(prot5_features)
-        prot5_features_df.to_csv(f'{output_dir}/prott5.csv', index=False)
+        prott5_features = [prott5_encoder.generate_prott5_embedding(sequence) for sequence in sequences]
+        prott5_features = np.array(prott5_features)
+        with h5py.File(f'{output_dir}/prott5.h5', 'w') as f:
+            f.create_dataset('prott5', data=prott5_features)
         print('Prott5 encoding done!')
 
     if not any(feature_type in ['one_hot', 'ifeatpro', 'esm1v', 'prott5', 'all'] for feature_type in feature_types):
